d75d4b3670a65f6d3e258b063f89bcb416a53949
Yusha Tahlil <ymmttmy5@gmail.com>
2024-04-29 20:05:33 +0530

    Added: Hook for dynamically detecting origin

--- providers/app-provider.tsx
+++ providers/app-provider.tsx
@@ -1,17 +1,19 @@
 "use client";
 
+import useOrigin from "@/hooks/useOrigin";
 import { trpc } from "@/utils/trpc";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import React, { ReactNode, useState } from "react";
 
 const AppProvider = ({ children }: { children: ReactNode }) => {
+  const origin = useOrigin();
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: `${origin}/api/trpc`,
         }),
       ],
     })
